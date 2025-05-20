18cd3bf64c9dbc69f8e72c29e4413ffc6c317595
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2025-05-20 21:48:27 +0300

    My second commit

---
+++ Valeria_Pavlovich/Class_work/Class_work_2/task1.py
@@ -0,0 +1,26 @@
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+total_sum = a + b
+total_subtract = a - b
+total_multiple = a * b
+total_divide = a / b
+print ('Sum:', total_sum)
+print ('Substraction:', total_subtract)
+print ('Multiplication:', total_multiple)
+print ('Division:', total_divide)
+
+if a==b:
+    print('The numbers are equal')
+else:
+    print('The numbers are not equal')
+
+if a!=b:
+    print('The numbers are not equal')
+else:
+    print('The numbers are equal')
+
+if a>b:
+    print('The first number is bigger than the second number')
+else:
+    print('The second number is bigger than the first number')
+
