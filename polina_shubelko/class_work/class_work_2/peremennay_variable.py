21eca5695e923e3e34b3f6ef0709e80bc1d6fee0
polinashubelko <subelkopolina@gmail.com>
2025-05-20 21:57:57 +0300

    Мой второй коммит

--- polina_shubelko/class_work/class_work_2/peremennay_variable.py
+++ polina_shubelko/class_work/class_work_2/peremennay_variable.py
@@ -6,3 +6,4 @@ print ('Привет, я Полина, мне 24 года')
 print ('Привет, я', NAME_USER, ', мне', AGE_USER,'года')
 print ('Привет, я ' + NAME_USER + ', мне ' + str(AGE_USER) + ' года')
 print (f'Привет, я {NAME_USER}, мне {AGE_USER} года')
+
