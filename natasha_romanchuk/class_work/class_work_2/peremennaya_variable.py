9fea6449fd6c40d2fef36bcd0b4037c7a821254f
natal <Natalka.shv@gmail.com>
2025-05-20 21:50:39 +0300

    Мой второй коммит

---
+++ natasha_romanchuk/class_work/class_work_2/peremennaya_variable.py
@@ -0,0 +1,9 @@
+NAME_USER = 'Наташа'
+AGE_USER = 35
+
+
+print('Привет, я Наташа, мне 35 лет')
+print('Привет,я' ,NAME_USER, ', мне' , AGE_USER, 'лет')
+print('Привет, я ' + NAME_USER, ', мне  ' +  str(AGE_USER), 'лет')
+print(f'Привет, я {NAME_USER},  мне {AGE_USER} лет')
+
