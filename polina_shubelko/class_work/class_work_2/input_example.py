efa604ff325f169c1d85a8f55103eabf85dc3a64
polinashubelko <subelkopolina@gmail.com>
2025-05-20 21:51:42 +0300

    Мой второй коммит

---
+++ polina_shubelko/class_work/class_work_2/input_example.py
@@ -0,0 +1,37 @@
+NAME_USER = input('Введите свое имя:')
+AGE_USER = input('Введите свой возраст:')
+
+print ('Привет, я Полина, мне 24 года')
+print ('Привет, я', NAME_USER, ', мне', AGE_USER,'года')
+print ('Привет, я ' + NAME_USER + ', мне ' + str(AGE_USER) + ' года')
+print (f'Привет, я {NAME_USER}, мне {AGE_USER} года')
+
+A = input ('Введите число A')
+B = input ('Введите число B')
+
+if A==B:
+    print ('Числа равны')
+else:
+    print ('Числа не равны')
+
+if A!=B:
+    print ('Числа не равны')
+else:
+    print ('Числа равны')
+
+if A > B:
+    print('>')
+else:
+    print('<')
+
+if A < B:
+    print('<')
+else:
+    print('>')
+
+A = input('Введите пароль')
+
+if ('A = 123456'):
+    print('Доступ разрешен')
+else:
+    print('Неверный пароль')
---
+++ polina_shubelko/class_work/class_work_2/peremennay_variable.py
@@ -0,0 +1,7 @@
+NAME_USER = 'Полина'
+AGE_USER = 24
+
+print ('Привет, я Полина, мне 24 года')
+print ('Привет, я', NAME_USER, ', мне', AGE_USER,'года')
+print ('Привет, я ' + NAME_USER + ', мне ' + str(AGE_USER) + ' года')
+print (f'Привет, я {NAME_USER}, мне {AGE_USER} года')
