7b0428f78bbe2a9a60df7aabc45ec33b9a4badb9
Irina <iren.bones@yandex.by>
2025-05-20 21:48:40 +0300

    Мой второй коммит

---
+++ bachko_irina/class_work_2/peremennaya_variable.py
@@ -0,0 +1,9 @@
+NAME_USER = 'Irina'
+AGE_USER = 40
+
+
+print('Привет, я Irina, мне 40 лет')
+print('Привет, я', NAME_USER , ',мне', AGE_USER , 'лет' )
+print('Привет, я '+ NAME_USER + ', мне ' + str(AGE_USER) + 'лет')
+print(f'Привет, я {NAME_USER}, мне {AGE_USER} лет')
+
