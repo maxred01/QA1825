da24f3b5700cc9f1cbf09620c818616fd72e4d0c
valery <wazgenby@gmail.com>
2025-05-20 21:48:49 +0300

    Мой второй коммит

---
+++ Valery_Hehenia/Class_work/Class_work_2/Input_example.py
@@ -0,0 +1,59 @@
+# # NAME_USER = input('Введите своё имя: ')
+# # AGE_USER = input('Введите свой возрост: ')
+# #
+# # print('Привет, я Валерий, мне 28 лет')
+# # print('Привет, я', NAME_USER, ', мне', AGE_USER, 'лет')
+# # print('Привет, я '+NAME_USER+ ', мне ' + str(AGE_USER)+'лет')
+# # print(f'Привет, я {NAME_USER}, мне {AGE_USER} лет')
+#
+# NUMBER_1= int(input('Введите число a: '))
+# NUMBER_2 = int(input('Введите число b: '))
+#
+# print('1.Задание со сложением')
+# print('a+b=', NUMBER_1+NUMBER_2)
+#
+# print('2.Задание с вычитанием')
+# print('a-b=', NUMBER_1-NUMBER_2)
+#
+# print('3.Задание с умножением')
+# print('a*b=', NUMBER_1*NUMBER_2)
+#
+# print('4.Задание с делением')
+# print('a/b=', NUMBER_1/NUMBER_2)
+#
+# print('5.Задание с сравнением(==)')
+# if NUMBER_1==NUMBER_2:
+#     print('Числа ровны')
+# else:
+#     print('Числа не ровны')
+#
+# print('6.Задание с сравнением (!=)')
+# if NUMBER_1!=NUMBER_2:
+#     print('Числа не ровны')
+# else:
+#     print('Числа ровны')
+#
+# print('7.Задание с сравнением (>)')
+# if NUMBER_1 > NUMBER_2:
+#     print('Верно')
+# else:
+#     print('Не верно')
+#
+# print('8.Задание с сравнением (<)')
+# if NUMBER_1 < NUMBER_2:
+#     print('Верно')
+# else:
+#     print('Не верно')
+
+PASSWORD_PAS=123456
+PASSWORD = int(input('Введите ваш пароль:'))
+if PASSWORD==PASSWORD_PAS:
+    print('Пароль верный, входи в систему')
+else:
+    print('Пароль не верный. Доступ закрыт!')
+
+
+GRADUSI_CILSII = int (input('Введите градусы Цельсия: '))
+GRADUSI_FARENGEIT = (GRADUSI_CILSII*9/5)+32
+
+print('Градусы цельсия: '+str(GRADUSI_CILSII)+ ' | Градусы по фаренгейту: ' + str(GRADUSI_FARENGEIT))
