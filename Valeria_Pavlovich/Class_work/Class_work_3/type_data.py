9034c63f140e5d13ea7e21adaa60803b3ad7d66d
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2025-05-22 21:43:36 +0300

    My third commit

---
+++ Valeria_Pavlovich/Class_work/Class_work_3/type_data.py
@@ -0,0 +1,21 @@
+num = 100500
+result_1 = str(num)
+print(result_1)
+
+num_fl = 12.3
+result_2 = int(num_fl)
+print(result_2)
+
+num_bool = True
+result_3 = int(num_bool)
+print(result_3)
+
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+my_dict = {'name': 'valeria', 'age': 26}
+res_tuple = tuple(my_list)
+res_list = list(my_tuple)
+print(res_tuple)
+print(res_list)
+print(my_dict.keys())
+print(list(my_dict.values()))
