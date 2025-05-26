4f7c0c4aba6cbe7b27ea6873d71a70ec9fdc2431
natal <Natalka.shv@gmail.com>
2025-05-26 13:08:38 +0300

    Мой второй коммит

---
+++ natasha_romanchuk/home_work/home_work_1/home_work_1.py
@@ -0,0 +1,64 @@
+# задача 1
+X= int(input("Введите число"))
+Y = str(X)
+print(f'"Число: {Y}"')
+
+
+# задача 2
+a = int(input("Введите число:"))
+b = int(input("Введите число:"))
+print(a + b)
+
+# задача 3
+numbers = [5, 10, 15]
+print(tuple(numbers))
+
+# задача 4
+word1 = input("Введите первое слово: ")
+word2 = input("Введите второе слово: ")
+results_join = " - ".join([word1, word2])
+print(results_join)
+
+# задача 5
+q = str(input("Введите целое число: "))
+w = int(q)
+print(w * 2)
+
+# задача 6
+fruits = ["apple", "banana", "cherry"]
+fruits_tuple = tuple(fruits)
+print(len(fruits_tuple))
+
+# задача 7
+e = float(input("Введите число с плавающей точкой: "))
+r = int(e)
+print(r)
+
+# задача 8
+name = input("Введите имя: ")
+age = input("Введите возраст: ")
+print(f"Имя: {name}, Возраст: {age}")
+
+# задача 9
+text = 'python'
+my_list = list(text)
+print(my_list)
+
+# задача 10
+numbers_1 = (1, 2, 3)
+result = ''.join(str(num) for num in numbers_1)
+print(result)
+
+# задача 11
+sentenсe = input("Введите предложение: ")
+words_1 = sentenсe.split()
+print(len(words_1))
+
+# задача 12
+words = ["Hello", "world", "!"]
+results_join = "  ".join(words)
+print(results_join)
+
+
+
+
