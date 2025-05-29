ec69271cfc043b5a8a57d8cccfe18fd063725a63
Dimitri Samosiuk <imsweet75@gmail.com>
2025-05-29 21:49:40 +0300

    Мой четвертый коммит

---
+++ natasha_romanchuk/class_work/class_work_5/logic.py
@@ -0,0 +1,58 @@
+# ПРЕОБРАЗОВАНИЕ ТИПОВ
+# Комбинированное преобразование
+
+#
+# num1 = '15'
+# num2 = '7.5'
+#
+# if num1.isdigit() and num2.replace('.', '',1).isdigit():  # isdigit cостоит ли только из цыфр (без .)  replase заменяет точку
+#     total = float(num1) + float(num2)
+#     print(f'Сумма {total}')
+# else:
+#     print('Ошибка')
+
+# Пример 2 булевые преобразования с условием
+
+# user_input = input('Хотите продолжить? (Да/Нет) ').lower()
+#
+# continue_program = True if user_input == 'да' else False
+#
+# if continue_program and 10 > 5:
+#     print('продолжаем работу')
+# else:
+#     print('завершаем работу')
+
+
+# УСЛОВИЯ И ИХ КОМБИНАЦИИ
+# Пример 1 Множественные условия с or
+
+# weather = input('Погода (солнце/дождь/снег)').lower()
+#
+# if weather == 'дождь' or  weather == 'снег':
+#     print('Возьмите зонт')
+# elif weather == 'солнце' and  int(input('уф-индекс')) > 5:
+#     print('Используй крем')
+# else:
+#     print('Улыбайся, хорошего дня')
+
+# ВЛОЖЕННЫЕ КОНСТРУКЦИИ
+# Пример 1 многоуровневая вложенность
+
+account_type = 'premium'
+balance = 15000
+withdraw = 20000
+
+if account_type == 'premium':
+    if balance >= withdraw:
+        print('выдача денег')
+    else:
+        overdraft = withdraw - balance
+        if overdraft <= 5000:
+            print('овердрафт одобрен')
+        else:
+            print('Превышен лимит')
+else:
+    if balance >= withdraw:
+        print('Выдача денег')
+    else:
+        print('Недостаточно средств')
