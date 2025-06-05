6c46bc84da3384d2365fb8f19b2e325cb4dba69d
Anastasia <anastasiakuprianchik0807@gmail.com>
2025-06-05 21:56:13 +0300

    Мой 7 коммит

--- nastya_kuprianchik/class_work/class_work5/request_get.py
+++ nastya_kuprianchik/class_work/class_work5/request_get.py
@@ -1,15 +1,49 @@
 import requests
+import pytest
 
-url = "https://petstore.swagger.io/v2/pet/findByStatus"
-status_pending = 'pending'
+def test_find_by_status():
+    url = "https://petstore.swagger.io/v2/pet/findByStatus"
+    status_pending = 'pending'
+    response = requests.request("GET", url, params={'status': status_pending})
+    results = response.json()
+    print(results)
+    results_name = results[0]['name']
+    assert response.status_code == 200, 'Сайт недоступен? Ожидался статус код 200'
+    #assert results_name == status_pending, f'Неверный статус в поле "status", а вывелось {results_name}'
+    # assert  'name' in results[0], 'ключ "name нет в ответе'
 
-response = requests.request("GET", url, params={'status': status_pending})
 
-results = response.json()
-print(results)
-results_name = results[0]['name']
+def test_inventory():
 
-assert response.status_code == 200, 'Сайт недоступен? Ожидался статус код 200'
-#assert results_name == status_pending, f'Неверный статус в поле "status", а вывелось {results_name}'
-assert  'name' in results[0], 'ключ "name нет в ответе'
+    url = 'https://petstore.swagger.io/v2/store/inventory'
+    response = requests.request('GET', url)
+    results = response.json()
 
+    assert results["7000"] == 1
+
+
+def test_inventory2():
+    for i in range(3):
+        url = 'https://petstore.swagger.io/v2/store/inventory'
+        response = requests.request('GET', url)
+        results = response.json()
+
+        if '7000' in results:
+            assert results["7000"] == 1, f"попытка {i+1}: значение равно {results["7000"]}"
+            print('проверка пройдена')
+            break
+        else:
+            print(f'попытка {i+1}: ключ "7000" не найден')
+    else:
+        assert False , 'ключ "7000" не найден после 3 попыток '
+
+def test_inventory3():
+    url = 'https://petstore.swagger.io/v2/store/inventory'
+    response = requests.request('GET', url)
+    results = response.json()
+
+    for key, value in results.items():
+        if key.isdigit():
+            print(f'проверка ключа {key} {value}')
+            if key == '7000':
+                assert value == 1 , f'значение для "7000" равно {value}'
