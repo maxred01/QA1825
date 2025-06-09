96a79c3a32be411d1fa8b3988e09ab6ea2fa2c72
Valeria Pavlovich <v.pavl.msq@gmail.com>
2025-06-09 21:31:01 +0300

    Home work 4 (from 08.06)

---
+++ Valeria_Pavlovich/Home_work/Home_work_4/exercise2.2.py
@@ -0,0 +1,12 @@
+﻿total_sum = 0
+total_count = 0
+while True:
+    number = int(input('Enter a number (enter 0 to stop): '))
+    if number == 0:
+        break
+    total_sum += number
+    total_count += 1
+    avg = total_sum / total_count
+print(f'Total sum is {total_sum}')
+print(f'Total count is {total_count}')
+print(f'Average is {avg:.2f}')
---
+++ Valeria_Pavlovich/Home_work/Home_work_4/exercise2.4.py
@@ -0,0 +1,12 @@
+﻿import time
+import winsound
+seconds_input = int(input('Enter time in seconds: '))
+start_time = time.time()
+for i in range(seconds_input, 0, -1):
+    minutes = i // 60
+    seconds = i % 60
+    print(f'Time: {minutes:02d}:{seconds:02d}')
+    winsound.Beep (1000, 300)
+    time.sleep(0.7)
+print('Start!')
+winsound.Beep (1000, 1000)
