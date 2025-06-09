82edf42c181366b4ce3f005b774ab1e406bbc601
natal <Natalka.shv@gmail.com>
2025-06-09 12:28:47 +0300

    мой 3 коммит дз

---
+++ natasha_romanchuk/class_work/class_work_7/tg_bot.py
@@ -0,0 +1,21 @@
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Привет: отправь мне ссылку на проверку')
+
+def check_url(updatr: Update, context: CallbackContext) -> None:
+    url = update.message.text
+
+    if not url.startwitch('http://', 'https://'):
+    url = 'http://' + url
+
+    try:
+    response = requests.head(url, allow_redirects=True, timeout=10)
+    status_code = response.status_code
+    update.message.text(f'Статус кода для сайта {url}: {status_code}')
+    except requests.exceptions.RequestException as e:
+    update.message.text(f'Ошибка проверки URL {str(e)}')
+
+
+def main() -> None:
+
+    updater = Update(TOKEN)
+    dispatcher = updater.d
