d4736f4d03faa82558b4772ed29b059cda550889
Valery <wazgenby@gmail.com>
2025-07-10 21:48:26 +0300

    commit class work 15

--- Valery_Hehenia/Class_work/Class_work_14/onliner_api/onliner_api.py
+++ Valery_Hehenia/Class_work/Class_work_14/onliner_api/onliner_api.py
@@ -5,18 +5,31 @@ class OnlinerAPI(APIClient):
     """Специализированный клиент для API Onliner.by"""
 
     def __init__(self):
+        #Вызов родительского метода с параметрами
         super().__init__("https://www.onliner.by/sdapi")
 
+
+    #Метод поиска продукта
     def search_products(self, query, **params):
-        """Поиск товаров в каталоге"""
+        """
+        Поиск товаров в каталоге
+        :param query:
+        :param params:
+        :return:
+        """
+
 
         parameters = {'query': query, **params}
         return self.get('catalog.api/search/products', params=parameters)
 
+
+    #Метод получения актуальных курсов
     def get_currency_rates(self):
         """Получение актуальных курсов валют"""
 
         return self.get('main.api/currency')
+
+
 
     def get_product_details(self, product_id):
         """Получение детальной информации о товаре"""
