ad94ca8b651231883d82ffa886447a74c74f3a53
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2025-07-17 22:05:41 +0300

    My 16th commit

--- Valeria_Pavlovich/Class_work/Class_work_14/class_14/site_belavia/api.py
+++ Valeria_Pavlovich/Class_work/Class_work_14/class_14/site_belavia/api.py
@@ -1,44 +1,13 @@
 from Valeria_Pavlovich.Class_work.Class_work_14.class_14.clients.api_client import APIClient
 
 
-class BelaviaAPI(APIClient):
-    """Специализированный клиент для API Belavia.by"""
+class HotelsB2API(APIClient):
+    """Специализированный клиент для API Hotels.belavia.by"""
 
     def __init__(self):
-        super().__init__("https://belavia.by/")
+        super().__init__("https://hotels.belavia.by/api")
 
-    def main_page(self):
-        """Стартовая страница"""
-        return self.get('/')
-
-    def buy_tickets(self):
-        """Купить билеты"""
-        return self.get('/#ibe')
-
-    def check_in(self):
-        """Регистрация на рейс"""
-        return self.get('/#wci')
-
-    def transfer(self):
-        """Трансфер"""
-        return self.get('/#transfer')
-
-    def hotel(self):
-        """Отели"""
-        return self.get('/#hotel')
-
-    def tickets_order(self):
-        """Заказ билетов"""
-        return self.get('/booking/')
-
-    def account(self):
-        """Личный кабинет"""
-        return self.get('/account/')
-
-    def news(self):
-        """Новости"""
-        return self.get('/novosti/')
-
-    def offers(self):
-        """Предложения"""
-        return self.get('/predlozheniya/')
+    def search(self, query, **params):
+        """Поиск отелей"""
+        parameters ={'query': query, **params}
+        return self.get('site/multicomplete.json', params=parameters)
--- Valeria_Pavlovich/Class_work/Class_work_14/class_14/site_belavia/schemas.py
+++ Valeria_Pavlovich/Class_work/Class_work_14/class_14/site_belavia/schemas.py
@@ -1,26 +1,35 @@
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import List
 
+class Hotel(BaseModel):
+    otahotel_id: str
+    master_id: int
+    hotel_name: str
+    region_id: int
+    region_name: str
+    country_code: str
+    country_name: str
+    hotel_kind: str
+    hotel_address: str
+    slug: str
+    region_name_en: str
+    country_name_en: str
+    is_natdis_critical: bool
 
-class AirportInfo(BaseModel):
-    cityId: int
-    airportCode: str
-    airportName: str
+class Region(BaseModel):
+    id: int
+    type: str
+    name: str
+    country: str
+    country_code: str
+    currency_code: str
+    pretty_slug: str
+    slug: str
+    is_sales_closed: bool
+    name_en: str
+    country_en: str
+    is_natdis_critical: bool
 
-
-class Segment(BaseModel):
-    departure: AirportInfo
-    arrival: AirportInfo
-    departureDate: str
-    flightNumber: str
-    serviceClass: str
-
-
-class PriceDetails(BaseModel):
-    amount: float
-    currency: str
-
-
-class Itinerary(BaseModel):
-    segments: List[Segment]
-    price: PriceDetails
+class BelaviaResponse(BaseModel):
+    hotels: List[Hotel]
+    regions: List[Region]
