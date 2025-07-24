3e8f601ca9e4274f9ca1f9cc0d2528d4e380d9b7
SimpleMax05 <onpustmacs@yandex.by>
2025-07-24 18:45:13 +0300

    commit class work 16

---
+++ stasya_voishnis/home_work/home_work_6/test_selenium_ex.py
@@ -0,0 +1,64 @@
+import time
+import allure
+from allure_commons.types import Severity
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import pytest_check as check
+
+@allure.title("Этот тест проверяет заполнение и результатт заполнения полей")
+@allure.description("""Тест вводит текст для полей user_name и тд и сверяет результат в полях name и тд""")
+@allure.tag("Smoke")
+@allure.severity('Severiti')
+@allure.label()
+@allure.id("123")
+@allure.manual
+@allure.link("https://hoster.by/", name="Тест кейсы теста")
+@allure.testcase("TMS-456")
+@allure.epic("UI автотесты")
+@allure.feature("Раздел Elements")
+@allure.story("Test Box")
+def test_selenium():
+
+    with allure.step('Подготовка тестовых данных'):
+        user_name = 'tri-kota'
+        user_email = 'test@gmail.com'
+        current_address = 'test currentAddress'
+        permanent_address = 'test permanentAddress'
+
+    with allure.step('Запускаем и настраеваем Браузер'):
+        driver = webdriver.Chrome()
+        driver.get("https://demoqa.com/text-box")
+        driver.maximize_window()
+    time.sleep(1)
+
+    elemets_form_send_keys = [
+        (driver.find_element(By.ID, 'userName'), 'Full Name', user_name),
+        (driver.find_element(By.ID, 'userEmail'), 'Email', user_email),
+        (driver.find_element(By.ID, 'currentAddress'), 'Current Address', current_address),
+        (driver.find_element(By.ID, 'permanentAddress'), 'Permanent Address', permanent_address)
+    ]
+    for element, text_elemet, send_keys_element in elemets_form_send_keys:
+        with allure.step(f'Ввод текста для поля {text_elemet}'):
+            element.send_keys(send_keys_element)
+
+    time.sleep(2)
+    with allure.step('Cкролл страницы вниз'):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    with allure.step('Нажатие кнопки Submit'):
+        driver.find_element(By.ID, 'submit').click()
+
+    elemets_form = [
+        (driver.find_element(By.ID, 'name'), user_name), (driver.find_element(By.ID, 'email'), user_email),
+        (driver.find_element(By.XPATH, '//p[@id="currentAddress"]'), current_address),
+        (driver.find_element(By.XPATH, '//p[@id="permanentAddress"]'), permanent_address)
+    ]
+    for elemet, elemets_text, name_from in elemets_form:
+        with allure.step(f'Проверка поля{name_from}'):
+            check.greater(elemet.text.find(elemets_text), -1, f"Текста {elemets_text} нет на экарне")
+
+    time.sleep(2)
+    driver.quit()
