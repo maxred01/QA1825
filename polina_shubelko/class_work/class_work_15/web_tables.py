87b0672c7fbd40d5c3e198ba253b076a97f47df2
polinashubelko <subelkopolina@gmail.com>
2025-08-05 23:19:38 +0300

    class_work_15

--- polina_shubelko/class_work/class_work_15/web_tables.py
+++ polina_shubelko/class_work/class_work_15/web_tables.py
@@ -37,7 +37,7 @@ def test_selenium_web_tables():
         elements = driver.find_elements(By.CSS_SELECTOR, 'span[title="Delete"]')
         driver.find_element(By.CSS_SELECTOR, 'span[title="Delete"]').click()
         elements_delete = driver.find_elements(By.CSS_SELECTOR, 'span[title="Delete"]')
-        time.sleep(3)
+        time.sleep(2)
         check.not_equal(elements, elements_delete)
         time.sleep(2)
 
@@ -58,25 +58,26 @@ def test_selenium_web_tables():
             (driver.find_element(By.XPATH, '//input[@pattern="\d*"]'), 'Age', Age),
             (driver.find_element(By.XPATH, '//input[@placeholder="Salary"]'), 'Salary', Salary),
             (driver.find_element(By.XPATH, '//input[@placeholder="Department"]'), 'Department', Department)
-
         ]
         for element, name_form, input_value in elements_form_send_keys:
             with allure.step(f'Заполнение поля {name_form}'):
                 element.clear()
                 element.send_keys(input_value)
-                time.sleep(2)
+
             with allure.step(f'Проверка текста в поле {name_form}'):
                 text_in_element = element.get_attribute('value')
                 index = text_in_element.find(input_value)
                 check.greater(index, -1, f"Текста {input_value} нет в поле {name_form}")
-                time.sleep(2)
+                time.sleep(1)
 
                 driver.find_element(By.XPATH, '// button[ @ id = "submit"]').click()
+
+    time.sleep(2)
 
     with allure.step('Проверка редактирования записи'):
         elements_before = driver.find_elements(By.CSS_SELECTOR, 'span[title="Delete"]')
         driver.find_element(By.CSS_SELECTOR, 'span[title="Edit"]').click()
-        time.sleep(3)
+        time.sleep(2)
         elements_after = driver.find_elements(By.CSS_SELECTOR, 'span[title="Edit"]')
         check.not_equal(elements_before, elements_after)
         time.sleep(2)
@@ -95,7 +96,6 @@ def test_selenium_web_tables():
             (driver.find_element(By.XPATH, '//input[@pattern="\d*"]'), 'Age', Age),
             (driver.find_element(By.XPATH, '//input[@placeholder="Salary"]'), 'Salary', Salary),
             (driver.find_element(By.XPATH, '//input[@placeholder="Department"]'), 'Department', Department)
-
         ]
 
         for locator, name_form, input_value in elements_form_send_keys:
@@ -104,16 +104,14 @@ def test_selenium_web_tables():
             )
             element.clear()
             element.send_keys(input_value)
-            time.sleep(2)
+            time.sleep(1)
 
         with allure.step(f'Проверка текста в поле {name_form}'):
                 text_in_element = element.get_attribute('value')
                 index = text_in_element.find(input_value)
                 check.greater(index, -1, f"Текста {input_value} нет в поле {name_form}")
-                time.sleep(2)
 
                 driver.find_element(By.XPATH, '// button[ @ id = "submit"]').click()
+                time.sleep(2)
 
-
-        time.sleep(2)
         driver.quit()
