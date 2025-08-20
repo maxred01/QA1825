a40122b8f6668b732b86b05cd3b10045d58a5d12
natal <Natalka.shv@gmail.com>
2025-08-20 08:18:45 +0300

    Мой 7 коммит дз

---
+++ natasha_romanchuk/class_work/class_work_13/locators/locators_button.py
@@ -0,0 +1,5 @@
+class LocatorsButton:
+    double_click_btn = 'doubleClickBtn'
+    right_click_btn = 'rightClickBtn'
+    upload_click_btn = 'uploadFile'
+    double_click_btn_message = 'doubleClickMessage'
