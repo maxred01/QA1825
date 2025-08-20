c1988c973682a12bc857af2bbadcc13398d8fab1
natal <Natalka.shv@gmail.com>
2025-08-20 08:31:34 +0300

    Мой 7 коммит home

--- natasha_romanchuk/home_work/home_work_7/test_webtables.py
+++ natasha_romanchuk/home_work/home_work_7/test_webtables.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 import pytest_check as check
 
 
-
 @allure.title("Тест Web Tables")
 @allure.description("Проверка добавления новой записи")
 @allure.tag("Regression")
