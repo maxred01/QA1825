7e46936d1d8036e5d65b3a6877759e3c42bce8fd
natal <Natalka.shv@gmail.com>
2025-08-20 13:16:19 +0300

    Мой 7 коммит home

--- natasha_romanchuk/class_work/class_work_7/tests/UI/test_selenium_heder.py
+++ natasha_romanchuk/class_work/class_work_7/tests/UI/test_selenium_heder.py
@@ -1,13 +1,7 @@
-import time
 import allure
-from allure_commons.types import LabelType # импорт специального перечисления из allure(epic,feature,story,severity...)
-from selenium import webdriver
 from selenium.webdriver.common.by import By # стратегии поиска элементов
-from selenium.webdriver.common.keys import Keys # виртуальное представление клавиш клавиатуры
-from selenium.webdriver.common.action_chains import ActionChains # двойной клик,выпадающие меню, перетаскиваемые элементы, элементы с hover-эффектами и других
-import pytest_check as check # предоставляет функционал "мягких assertions" (soft assertions) для тестирования в Python
 from natasha_romanchuk.class_work.class_work_7.conftest import web_browser
-from natasha_romanchuk.class_work.class_work_7.tests.Locators.locator_button import LocatorButton
+from natasha_romanchuk.class_work.class_work_7.Locators.locator_button import LocatorButton
 
 @allure.feature("Header Navigation")
 @allure.story("Visual Elements Display")
@@ -25,5 +19,5 @@ def test_header_logo_displayed(web_browser):
         assert logo.is_displayed()
 
     with allure.step('Отображение иконки поиска'):
-        search  = driver.find_element(By.XPATH, LocatorButton.search_button)
+        search_btn  = driver.find_element(By.XPATH, LocatorButton.search_button)
         assert logo.is_displayed()
