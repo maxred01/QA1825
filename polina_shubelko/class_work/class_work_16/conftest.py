33a8e562800fc9066300df1cdc8ab4677bc97a01
polinashubelko <subelkopolina@gmail.com>
2025-08-21 21:40:29 +0300

    Мой семнадцатый коммит

--- polina_shubelko/class_work/class_work_16/conftest.py
+++ polina_shubelko/class_work/class_work_16/conftest.py
@@ -3,6 +3,7 @@ import pytest
 import allure
 from selenium import webdriver
 import pytest_check as check
+from QA1825.polina_shubelko.class_work.class_work_16.page.base_page import WebPage
 
 @pytest.fixture
 def web_browser(request):
@@ -11,15 +12,12 @@ def web_browser(request):
 
     yield browser
 
-    if request.node.rep_call.failed:
-        try:
-            allure.attach(browser.get_screenshot_as_png(),
-                          name = f'скрин экрана {request.node.name}',
-                          attachment_type = allure.attachment_type.PNG)
-        except:
-            pass
-
-
+    try:
+        allure.attach(browser.get_screenshot_as_png(),
+                      name = f"скрин {request.node.name}",
+                      attachment_type = allure.attachment_type.PNG)
+    except:
+        pass
 
 
     browser.quit()
