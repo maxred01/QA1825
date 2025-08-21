72a23c79733cafc24c09d52d5144222c54a94e2d
Irina <iren.bones@yandex.by>
2025-08-21 15:08:27 +0300

    my 5 home commit

--- bachko_irina/class_work_7/locators/locators_buttons.py
+++ bachko_irina/class_work_7/locators/locators_buttons.py
@@ -3,3 +3,4 @@ class LocatorsButton:
     shop = '//a[@href="https://shop.spacex.com"]'
     search = '//button[@class="Heading Link Link--primary Text--subdued u-h8"]'
     enter = '//input[@id="search-input"]'
+    
