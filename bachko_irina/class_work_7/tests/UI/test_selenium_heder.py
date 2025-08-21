6025a44278ed54010c66544a87bf3c59c345076b
Irina <iren.bones@yandex.by>
2025-08-21 15:08:11 +0300

    my 5 home commit

--- bachko_irina/class_work_7/tests/UI/test_selenium_heder.py
+++ bachko_irina/class_work_7/tests/UI/test_selenium_heder.py
@@ -5,6 +5,8 @@ from allure_commons.types import Severity, LabelType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.devtools.v136.dom import get_attributes
+from selenium.webdriver.common.devtools.v136.log import clear
 from selenium.webdriver.common.keys import Keys
 from bachko_irina.class_work_7.conftest import web_browser
 import pytest_check as check
@@ -33,21 +35,17 @@ def test_heder_search(web_browser):
         search = driver.find_element(By.XPATH, LocatorsButton.shop)
         assert search.is_displayd()
         driver.find_element(By.XPATH, LocatorsButton.search).click()
-        enter = driver.find_element(By.XPATH, '//input[@id="search-input"]').send_keys('abc')
+        enter_fild = driver.find_element(By.XPATH, '//input[@id="search-input"]').send_keys('abc')
+        print(enter_fild, get_attributes("abc"))
         time.sleep(2)
-
-
-
-driver.find_element(By.XPATH,  '//button[@class="Heading Link Link--primary Text--subdued u-h8"]').click()
-
-driver.find_element(By.XPATH, '//input[@id="search-input"]').send_keys('abc')
-
+        enter_fild = clear()
+        enter_fild = driver.find_element(By.XPATH, '//input[@id="search-input"]').send_keys('shits')
 
 
 time.sleep(5)
 
 # Закрыть браузер
-        driver.quit()
+driver.quit()
 
 
 
