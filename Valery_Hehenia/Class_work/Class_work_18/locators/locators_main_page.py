0e7dd663d5a9308b144e3acfdcf2de94de7ec2a7
Valery Hehenia <152875803+genady21@users.noreply.github.com>
2025-09-03 23:45:53 +0300

    commit class work 20 add categories, carusel

--- Valery_Hehenia/Class_work/Class_work_18/locators/locators_main_page.py
+++ Valery_Hehenia/Class_work/Class_work_18/locators/locators_main_page.py
@@ -20,6 +20,7 @@ class MainPage(WebPage):
     login_btn = WebElement(css_selector = '[data-target="#logInModal"]')
     sugnup_btn = WebElement(css_selector = '[data-target="#signInModal"]')
 
+
     #################Footer##################
 
 
@@ -30,4 +31,26 @@ class MainPage(WebPage):
     phone_text = WebElement(xpath = '//p[contains(text(),"Phone:")]')
     email_text = WebElement(xpath = '//p[contains(text(),"Email:")]')
     product_text = WebElement(xpath = '//h4[contains(text(),"PRODUCT STORE")]')
+
+
+ #################Categories##################
+
+
+    cat_block = WebElement(xpath = "//div[@class='list-group']")
+    cat_phones = WebElement(xpath = """//a[@onclick="byCat('phone')"]""")
+    cat_laptops = WebElement(xpath = """//a[@onclick="byCat('notebook')"]""")
+    cat_monitors = WebElement(xpath = """//a[@onclick="byCat('monitor')"]""")
+
+
+ #################Carusel##################
+
+
+    carusel_block = WebElement(id = "carouselExampleIndicators")
+    carusel_btn_next = WebElement(xpath = '//span[@class="carousel-control-next"]')
+    carusel_btn_previous = WebElement(xpath = '//a[@class="carousel-control-prev"]')
+
+    carusel_img_samsung = WebElement(xpath = '//img[@src="Samsung1.jpg"]')
+    carusel_img_nexus = WebElement(xpath = '//img[@src="nexus1.jpg"]')
+    carusel_img_iphone = WebElement(xpath = '//img[@src="iphone1.jpg"]')
+
 
---
+++ Valery_Hehenia/Class_work/Class_work_18/tests/UI/test_main_page.py
@@ -0,0 +1,136 @@
+﻿import time
+import allure
+import pytest_check as check
+
+from Valery_Hehenia.Class_work.Class_work_18.locators.locators_main_page import MainPage
+
+
+
+@allure.feature("Главная страница")
+@allure.story("Хедер")
+def test_headers(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+            (driver.logo_btn, 'Лого'),
+             (driver.home_btn, 'Home'),
+             (driver.contact_btn, 'Contact'),
+             (driver.about_btn, 'About'),
+             (driver.cart_btn, 'Cart'),
+             (driver.login_btn, 'Log in'),
+             (driver.sugnup_btn, 'Sign un'),
+
+
+                    ]
+
+    with allure.step('Проверка элемента'):
+        for element, text_element in elements:
+            with allure.step(f'Проверка элемента {text_element} на отображение'):
+                check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально на экарне')
+
+            with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+                check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+
+@allure.feature("Главная страница")
+@allure.story("Футер")
+def test_footers(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+             (driver.about_text, 'About US'),
+             (driver.text_text, 'Текст'),
+             (driver.get_intouch_text, 'Get in Touch'),
+             (driver.address_text, 'Address'),
+             (driver.phone_text, 'Phone'),
+             (driver.email_text, 'Email'),
+             (driver.product_text, 'PRODUCT STORE'),
+                    ]
+
+    with allure.step('Проверка элемента'):
+        for element, text_element in elements:
+            with allure.step(f'Проверка элемента {text_element} на отображение'):
+                check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально на экарне')
+
+            with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+                check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+
+
+@allure.feature("Главная страница")
+@allure.story("Категории")
+def test_categories(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+
+        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+             (driver.cat_block, 'Блок Categories'),
+             (driver.cat_phones, 'Phones'),
+             (driver.cat_laptops, 'Laptops'),
+             (driver.cat_monitors, 'Monitors'),
+                    ]
+
+    with allure.step('Проверка элемента'):
+        for element, text_element in elements:
+            with allure.step(f'Проверка элемента {text_element} на отображение'):
+                check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально на экарне')
+
+            with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+                check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+
+
+@allure.feature("Главная страница")
+@allure.story("Карусель")
+def test_carusel(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+
+        time.sleep(1)
+
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+             (driver.carusel_block, 'Блок Карусели'),
+             (driver.carusel_btn_next, 'Кнопка вперед'),
+             (driver.carusel_btn_previous, 'Кнопка назад'),
+                    ]
+
+    # with allure.step('Проверка элемента'):
+    #     for element, text_element in elements:
+    #         with allure.step(f'Проверка элемента {text_element} на отображение'):
+    #             check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально на экарне')
+    #
+    #         with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+    #             check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+
+
+    with allure.step('Проверка слайда по стрелке вправо'):
+        slides = [MainPage.carusel_img_samsung, MainPage.carusel_img_nexus, MainPage.carusel_img_iphone]
+
+        for i, slide in slides:
+
+            active_slide = web_browser.find_element(*slide)
+            assert active_slide.is_displayed(), f"Слайд {i + 1} не отображается!"
+            print(f"Слайд {i + 1} отображается: {slide}")
+
+
+            if i < len(slides) - 1:
+                next_btn = web_browser.find_element(MainPage.carusel_btn_next)
+                next_btn.click()
+                time.sleep(1)
