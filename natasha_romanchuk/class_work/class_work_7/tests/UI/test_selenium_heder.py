87339e229cf16f5ff500557af6d131d6982eba05
natasharomancuk <natalka.shv@gmail.com>
2025-09-05 19:42:36 +0300

    frame

--- natasha_romanchuk/class_work/class_work_7/tests/UI/test_selenium_heder.py
+++ natasha_romanchuk/class_work/class_work_7/tests/UI/test_selenium_heder.py
@@ -169,8 +169,30 @@ def test_vkorzine(web_browser):
             except:
                 print(f"Кнопка {i} НЕ кликабельна ")
 
+@allure.feature('Главная страница')
+@allure.story('Переход на сраницу "Контакты"через футер')
+def test_kontakty(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
 
-    # with allure.step('Отображение логотипа'):
+    with allure.step("Скроллим страницу вниз"):
+        driver._web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    with allure.step('Нажимаем на кнопку "Контакты" в футуре'):
+        driver.contacty_button.click()
+    with allure.step("Проверяем, что открылась страница Контакты"):
+        current_url = "https://emall.by/information/company/contacts"
+        # print("Текущий URL:", current_url)
+        assert current_url == "https://emall.by/information/company/contacts", \
+            f"Ожидали страницу Контакты, а открылась {current_url}"
+
+
+
+
+
+        # with allure.step('Отображение логотипа'):
     #     logo = driver.find_element(By.XPATH, driver.logotip)
     #     assert logo.is_displayed()
     #
